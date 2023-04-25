c930797daacce8a7bf8909af266b7eb9f5ebf0fc
Dmitry Mukhin <91732046+kaizxr@users.noreply.github.com>
2023-04-25 13:57:55 +0900

    import code from dpr

---
+++ fromdpr.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+nmax=700
+eps=1E-8
+
+'''
+v=array[0..nmax]  of extended;
+m=array[0..nmax,0..nmax]  of extended;
+'''
+
+def ch(x:float)->float:
+	return 0.5*(np.exp(x) + np.exp(-x))
+
+class alg:
+	def __init__(self) -> None:
+		self.x = np.zeros(nmax)
+		self.f = np.zeros(nmax)
+		self.y = np.zeros(nmax)
+		self.y0 = np.zeros(nmax)
+		self.b = np.zeros(nmax)
+		self.A = np.zeros(nmax)
+
+		self.c = np.zeros((nmax,nmax))
+		self.c1 = np.zeros((nmax,nmax))
+
+		self.n = 0
+		self.b0 = 0.0
+
+
+	def init(self):
+		self.n = 21
+		self.b0 = 1.5
+		
+		bj = 1
+		ap = 0.0
+		a = 1.0
+		for j in range(1,nmax):
+			self.b[j] = bj
+			bj = bj*self.b0
+			if j > 1:
+				a = a*(2*j-3)*(2*j-2)
+			self.c[j,j]= a
+			ap = a
+			for p in range(1,nmax-j):
+				ap = ap*(2*j-2+2*p-1)*(2*j-2+2*p)/((2*p-1)*(2*p))
+				self.c[j,j+p] = ap
+		for i in range(1,nmax):
+			self.b[i] =self.b[i]/self.c[i,i]
+			for j in range(1,i):
+				self.c1[i,j] =self.c[j,i]/self.c[j,j]
+	def _print(self, x : np.ndarray):
+		print(f'n={self.n} b={self.b0}')
+		for i in range(1,self.n):
+			print(x[i])
+		print('\n\n')
+
+	def solve(self):
+		zA = 0
+		xs = 0.0
+		r = 0.0
+
+		self.A[0] = 1
+		maxjp = 0
+		for j in range(1,self.n):
+			p = 0
+			z = 1
+			#repeat
+			while True:
+				if p>0:
+					self.A[p] = 0
+				zA = z
+				for k in range(p-1):
+					zA = -zA
+					self.A[p] = self.A[p]+zA*self.c1[j+p,j+k]*self.A[k]
+				xs = self.x[j]
+				self.x[j] = xs+z*self.A[p]*self.b[j+p]
+				r = abs(xs-self.x[j])
+				p += 1
+				z = -z
+				if maxjp<j+p:
+					maxjp=j+p
+
+				#until
+				if (r<eps) or (j+p>nmax):
+					break
+		print(f'j+p={maxjp}')
+	
+asd = alg()
+asd.init()
+# print(asd.c)
+# print(asd.c1)
+# print(asd.b)
+asd.solve()
+asd._print(asd.x)
+r = 1.0
+for i in range(1,10):
+	print(r/ch(np.sqrt(asd.b0)))
+	r = r*asd.b0/((2*i-1)*2*i)
+
+
