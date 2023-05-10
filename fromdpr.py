d5ff5e484a42c929cfdab49f5851b6bcced7851a
Dmitry Mukhin <91732046+kaizxr@users.noreply.github.com>
2023-05-10 22:59:36 +0900

    use zero indexing

--- fromdpr.py
+++ fromdpr.py
@@ -37,25 +37,25 @@ class alg:
 		bj = 1
 		ap = 0.0
 		a = 1.0
-		for j in range(1,nmax):
+		for j in range(nmax):
 			self.b[j] = bj
 			bj = bj*self.b0
-			if j > 1:
-				a = a*(2*j-3)*(2*j-2)
+			if j > 0:
+				a *= (2*j-1)*(2*j)
 			self.c[j,j]= a
 			ap = a
 			for p in range(1,nmax-j):
-				ap = ap*(2*j-2+2*p-1)*(2*j-2+2*p)/((2*p-1)*(2*p))
+				ap *= (2*j+2*p-1)*(2*j+2*p)/((2*p-1)*(2*p))
 				self.c[j,j+p] = ap
-		for i in range(1,nmax):
-			self.b[i] =self.b[i]/self.c[i,i]
-			for j in range(1,i):
+		for i in range(nmax):
+			self.b[i] = self.b[i]/self.c[i,i]
+			for j in range(i + 1): # necessary inclusive step
 				self.c1[i,j] =self.c[j,i]/self.c[j,j]
 
 
 	def _print(self, x : np.ndarray):
 		print(f'n={self.n} b={self.b0}')
-		for i in range(1,self.n):
+		for i in range(self.n):
 			print(x[i])
 		print('\n\n')
 
@@ -67,7 +67,7 @@ class alg:
 
 		self.A[0] = 1
 		maxjp = 0
-		for j in range(1,self.n):
+		for j in range(self.n):
 			p = 0
 			z = 1
 			#repeat
@@ -75,9 +75,9 @@ class alg:
 				if p>0:
 					self.A[p] = 0
 				zA = z
-				for k in range(p):
+				for k in range(p): # necessary inclusive step
 					zA = -zA
-					self.A[p] = self.A[p]+zA*self.c1[j+p,j+k]*self.A[k]
+					self.A[p] += zA*self.c1[j+p,j+k]*self.A[k]
 				xs = self.x[j]
 				self.x[j] = xs+z*self.A[p]*self.b[j+p]
 				r = abs(xs-self.x[j])
@@ -94,19 +94,29 @@ class alg:
 
 asd = alg()
 asd.init()
+
 # print(f'c:\n{asd.c}\n\n')
+
+print(f'c:\n')
+for i in range(10):
+	for j in range(10):
+		print(asd.c[i,j], end=' ')
+	print()
+print(f'c:\n')
+
 print(f'c1:\n')
 for i in range(10):
 	for j in range(10):
 		print(asd.c1[i,j], end=' ')
 	print()
 print(f'c1:\n')
-# print(f'b:\n{asd.b}\n\n')
+
 asd.solve()
 asd._print(asd.x)
 r = 1.0
-for i in range(1,11):
+for i in range(asd.n):
+	if i:
+		r *= asd.b0/((2*i-1)*2*i)
 	print(r/ch(np.sqrt(asd.b0)))
-	r = r*asd.b0/((2*i-1)*2*i)
 
 
