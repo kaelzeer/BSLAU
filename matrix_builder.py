91ea814ea2d5bbac845414eabac8a93a0b4409b6
Dmitry Mukhin <91732046+kaizxr@users.noreply.github.com>
2023-05-31 10:17:37 +0900

    WIP matrix 5

--- matrix_builder.py
+++ matrix_builder.py
@@ -1,5 +1,5 @@
 from utils import Utils
-from math import perm
+import math
 from time_logger import Time_logger
 import numpy as np
 
@@ -52,7 +52,7 @@ class Matrix_builder:
 			for p in range(algorithm.limit - j):
 				n = 2 * j + 2 * p + 1
 				k = 2 * j
-				sss = perm(n, k)
+				sss = math.perm(n, k)
 				algorithm.a[j,j+p] = sss
 			algorithm.f[j] = algorithm.b0 ** j
 			if j != 0:
@@ -109,7 +109,7 @@ class Matrix_builder:
 			for p in range(algorithm.limit - j):
 				n = 2 * j + 2 * p
 				k = 2 * j
-				sss = perm(n, k)
+				sss = math.perm(n, k)
 				algorithm.a[j,j+p] = sss
 			algorithm.f[j] = algorithm.b0 ** j
 			if j != 0:
@@ -131,3 +131,26 @@ class Matrix_builder:
 
 		print('lower_triangle * matrix:')
 		Utils.print_mat(algorithm.a, algorithm.f)
+
+	@staticmethod
+	def build_fifth_matrix(algorithm) -> None:
+		
+		algorithm.f = np.zeros(algorithm.limit)
+		algorithm.a = np.zeros((algorithm.limit,algorithm.limit),np.double)
+
+		n = 1 # temp
+		phi = 1 # algorithm.b5 / algorithm.a5
+		n_pi_phi = n * math.pi # * phi
+		alpha_n = (n * math.pi / 16) * (n_pi_phi + math.sinh(n_pi_phi)) / pow(math.cosh(n_pi_phi / 2), 2)
+		# alpha_n =  pow(phi, -5) * (n * math.pi / 16) * (n_pi_phi + math.sinh(n_pi_phi)) / pow(math.cosh(n_pi_phi / 2), 2)
+		alpha_after_10 = n * math.pi / 8
+
+		for p in range(1,algorithm.limit,2):
+			for n in range(1,algorithm.limit,2):
+				if n < 10:
+					alpha_n = (n * math.pi / 16) * (n_pi_phi + math.sinh(n_pi_phi)) / pow(math.cosh(n_pi_phi / 2), 2)
+				else:
+					alpha_n = n * math.pi / 8
+				algorithm.a[p,n] = alpha_n
+
+
